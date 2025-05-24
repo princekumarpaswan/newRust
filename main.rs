c483b63a287e2405c5674c82475443a2e660905a
princekumarpaswan <prasantandal123@gmail.com>
2025-05-24 18:41:58 +0530

    mutable expressions

--- main.rs
+++ main.rs
@@ -1,3 +1,17 @@
-fn main(){
-println!("hello Prince, I am rusthello Prince, I am rust")
+fn main() {
+    println!("hello Prince, I am rust");
+    test_fun();
+}
+
+fn test_fun() {
+    let x: f32 = 1093179847120371203.0;
+    let y: u8 = x as u8 - 5;
+    let mut iamold: bool = false;
+    println!("y: {:?}, iamold: {:?}", y, iamold);
+    iamold = true;
+    println!("iamold: {:?}", iamold);
+
+    let mystr: &str = "Apple";
+    println!("mystr: {}", mystr);
+
 }
