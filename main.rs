71d11dc01c1aa002558b229a22d108bfda1bba86
princekumarpaswan <prasantandal123@gmail.com>
2025-05-26 11:28:33 +0530

    format macro and string object

--- main.rs
+++ main.rs
@@ -1,25 +1,33 @@
 fn main() {
     println!("hello Prince, I am rust");
-    test_fun();
+    // test_fun();
+    println!("{}",gte_full_name("Prince","kumar"));
 }
 
-fn test_fun() {
-    let x: f32 = 10.0;
-    let y: u8 = x as u8 - 5;
-    let mut iamold: bool = false;
-    println!("y: {:?}, iamold: {:?}", y, iamold);
-    iamold = true;
-    println!("iamold: {:?}", iamold);
+fn gte_full_name(first :&str, last:&str) -> String {
+    // let full_name: String =
+     format!("{0} {1}", first, last)
+    // return  full_name;
+}
 
-    let mystr: &str = "Apple";
-    println!("mystr: {}", mystr);
 
-    let name: (&str, &str, u8) = ("Prince", "Kumar",22);
-    println!("name: {:?}", name);
+// fn test_fun() {
+//     let x: f32 = 10.0;
+//     let y: u8 = x as u8 - 5;
+//     let mut iamold: bool = false;
+//     println!("y: {:?}, iamold: {:?}", y, iamold);
+//     iamold = true;
+//     println!("iamold: {:?}", iamold);
 
-    let  array1: [u16; 6] = [ 23, 34, 45, 56, 67, 78];
-    println!("array1: {:?}", array1);
-    let new_ages: &[u16] = &array1[2..=4];
-    println!("new_ages: {:?}", new_ages);
+//     let mystr: &str = "Apple";
+//     println!("mystr: {}", mystr);
+
+//     let name: (&str, &str, u8) = ("Prince", "Kumar",22);
+//     println!("name: {:?}", name);
+
+//     let  array1: [u16; 6] = [ 23, 34, 45, 56, 67, 78];
+//     println!("array1: {:?}", array1);
+//     let new_ages: &[u16] = &array1[2..=4];
+//     println!("new_ages: {:?}", new_ages);
     
-}
+// }
